ae9f73a7957337e04c5ba2595569b9bcc36abb55
CuongNguyen3110 <cuongnguyen1.fit@gmail.com>
2018-11-05 13:58:53 +0700

    intial commit

--- lab1/homework/ex2.py
+++ lab1/homework/ex2.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
 uri = "mongodb://admin:admin@ds021182.mlab.com:21182/c4e"
 client = MongoClient(uri)
-db = client.get_database
+db = client.get_database()
 posts = db["post"]
 new_post = {
-    "Title": 
-    "Author": "Nguyen Manh Cuong"
-    "Content":
+    "Title": "C4E",
+    "Author": "Nguyen Manh Cuong",
+    "Content": "Hi vong minh se yeu thich code va hoan thanh tot khoa hoc",
 }
 posts.insert_one(new_post)
 client.close()
